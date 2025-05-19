70d893f80b2ea49192e06755b355ef8f78ce1fcb
Ben Devos <bendevos07@gmail.com>
2025-05-19 09:47:03 +0200

    task: added data.js

---
+++ kaart/data.js
@@ -0,0 +1,20 @@
+const locations = [
+    [
+        0,
+        "firstaid",
+        //new THREE.Vector3(firstAidCo.x, 0.45, firstAidCo.z),
+        {
+            lat: 50.98548325417922, 
+            lon: 4.516110690361381
+        },
+        "./assets/models/firstAidPin.glb",
+        true,
+        1,
+        {
+            name: "EHBO",
+            openingHours: null,
+            description: "Hier kan je terecht voor eerste hulp bij ongevallen. Bij een ongeval, bel 112.",
+            url: null
+        }
+    ],
+];
